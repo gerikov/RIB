6e826f74cdfe8375212a0f9e4645e6b741c5922e
gerikov <gerikov.mining@gmail.com>
2023-04-29 21:56:36 +0200

    FE - bg-fix

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,7 +3,7 @@
 const Hero = () => {
   return (
     <section className=' '>
-      <div className='bg-[url("./images/wazaa.jpg")] bg-cover bg-top md:screen50vh h-screen'>
+      <div className='bg-[url("images/wazaa.jpg")] bg-cover bg-top md:screen50vh h-screen'>
         <div className='bg-gradient-to-b from-black to-transparent md:screen50vh h-screen flex justify-center '>
           <div className='flex items-center  h-full max-w-5xl w-full justify-start px-4'>
             <h2 className='md:text-6xl text-4xl text-yellow-50 uppercase leading-relaxed font-patua '>
--- client/src/components/OrderForm.jsx
+++ client/src/components/OrderForm.jsx
@@ -1,22 +1,39 @@
 const OrderForm = ({ setIsOpenForm }) => {
-  const handleSubmit = (
-    
-  ) => { 
-    
-   }
+  const handleSubmit = () => {};
 
   return (
     <>
       <div className='fixed  h-full w-full bg-gray-950 left-0 z-30 top-0  opacity-40'></div>
       <div className='fixed h-72 w-11/12 md:max-w-[600px]  bg-gray-100 p-6 rounded-3xl drop-shadow-2xl -translate-y-1/2 top-1/2  -translate-x-1/2 left-1/2 opacity-100 z-40'>
         <div onClick={() => setIsOpenForm((prev) => !prev)}>X</div>
-        <form onSubmit={handleSubmit}>
-          <label >
-            <input type='text' name="name"/>
+        <form onSubmit={handleSubmit} className='flex flex-col'>
+          <label className='flex flex-col'>
+            Név
+            <input type='text' name='name'  className="py-1 bg-tra"/>
           </label>
-          <label htmlFor=''>
-            <input type='text' />
+          <label className='flex flex-col'>
+            Email cím
+            <input type='text' name='email' className="py-1 bg-tra" />
           </label>
+          <label className='flex flex-col'>
+            Telefon szám
+            <input type='text' name='phone' className="py-1 bg-tra" />
+          </label>
+          <label className='flex flex-col'>
+            Létszám
+            <select name='numPeople' id=''>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+              <option value='6'>6</option>
+              <option value='7'>7</option>
+              <option value='8'>8</option>
+              <option value='9'>9</option>
+            </select>
+          </label>
+          <button type='submit'>Elküld</button>
         </form>
       </div>
     </>
